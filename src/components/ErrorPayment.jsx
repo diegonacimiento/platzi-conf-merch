edbf3e5179aa472de1985b314b00a0a1dd1fd8bc
“Diego <diegonacimiento18@gmail.com>
2023-03-18 21:26:49 -0300

    Changes in the design

--- src/components/ErrorPayment.jsx
+++ src/components/ErrorPayment.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ErrorPayment() {
+    const linkStyle = {
+        fontSize: '20px',
+        color: '#000',
+        textDecoration: 'underline',
+    }
     return (
         <div>
             <h1>Error en el pago</h1>
-            <Link to='/checkout/payment'>Intentar de nuevo</Link>
+            <p>
+                Lo sentimos, ha ocurrido algún error con el pago. 
+                <br /> <br />
+                Intente otra vez siguiendo el link de aquí abajo.
+            </p>
+            <Link to='/checkout/payment' style={linkStyle}>Intentar de nuevo</Link>
         </div>
     )
 }
