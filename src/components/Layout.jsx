84b456571549c787fadb640ddd35a3a203ec6424
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-09-21 00:05:54 -0300

    feat: added splash

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/components/Layout.css';
 import Header from './Header';
 import Footer from './Footer';
 
+const splash = document.getElementById("splash");
+
 export default function Layout({ children }) {
+  const main = useRef(null);
+  setTimeout(() => {
+    splash.setAttribute("style", "display: none");
+    main.current?.setAttribute("style", "position: relative");
+  }, 3000);
   return (
-    <div className="Main">
+    <div className="Main" ref={main}>
       <Header />
       {children}
       <Footer />
